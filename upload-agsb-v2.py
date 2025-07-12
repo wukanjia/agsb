765b1d9d62179a153d492fd51c1afa95b8018091
wukanjia <wukanjia@yandex.com>
2025-07-12 13:59:39 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+USER_NAME = "wukanjia"         # 用户名
+UUID = "59e9b257-edf6-47e1-b546-e7e368916f1c"                   # UUID，留空则自动生成
+PORT =                    # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiN2U2Y2E4Y2EzNDJlMzQxZGZhM2I2Y2YxYWI4N2UwMGUiLCJ0IjoiOWU4ZmY5MTItNzgwYi00MjhlLWE0YmYtYTkxYTJkMWVmYzI2IiwicyI6Ill6YzBZVEV6TkRVdE1qWXhOaTAwTlRJM0xUZzROMk10TkRZeE5qUXhNRGxpWmpVdyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
